6fa4f018b82eb8e33aebfee3bebd21360a40f500
BhojrajSingh130 <raj.bsr1998@gmail.com>
2023-05-12 14:25:52 +0530

    om

--- src/app/dynamic.service.ts
+++ src/app/dynamic.service.ts
@@ -11,11 +11,9 @@ export class DynamicService {
   constructor(private http: HttpClient) { }
   token = 'IrAuCxwLhBO49H7iqAxLpJ6ZRfK47C0Ia2DHFFZn';
   getDataFromApi(): Observable<any> {
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + this.token
-    });
-    return this.http.get('https://benepik.in/yorker/api/brandForm', { headers: headers });
+    const headers = new HttpHeaders().set('Authorization', this.token);
+    const option = {headers}
+    return this.http.get('https://benepik.in/yorker/api/brandForm', option);
   }
 }
 
